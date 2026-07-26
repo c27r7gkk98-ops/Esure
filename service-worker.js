a4c560af23705e959a9bc8a05db6293e5e4022ef
c27r7gkk98-ops <c27r7gkk98@privaterelay.appleid.com>
2026-07-26 02:26:26 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 
-const CACHE='esure-no-email-v12-fixed';
+const CACHE='esure-no-email-v13-mobile-fixed';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json','./icon-192.png','./icon-512.png','./esure-logo.png','./certificate-template.png','./signature.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
